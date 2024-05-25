c812fff70306ecddd62776e0da1fe48c4260bf1a
adamghill <adamghill@yahoo.com>
2024-05-25 19:31:38 -0400

    Set COMPRESS_OFFLINE if needed.

--- site/app.py
+++ site/app.py
@@ -2,9 +2,15 @@
 
 from coltrane import initialize
 from django.core.management import execute_from_command_line
+from sys import argv
 
 
-wsgi = initialize()
+is_compress_offline = False
 
-if __name__ == "__main__":
+if len(argv) > 1 and argv[1] == "compress":
+    is_compress_offline = True
+
+wsgi = initialize(COMPRESS_OFFLINE=is_compress_offline)
+
+if __name__ == "__main__":    
     execute_from_command_line()
