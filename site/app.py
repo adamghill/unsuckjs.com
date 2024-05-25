516581324a31f27ec33d401b6edc7b0caa817ad0
adamghill <adamghill@yahoo.com>
2024-05-25 19:31:13 -0400

    Remove setting env explicitly.

--- site/app.py
+++ site/app.py
@@ -3,14 +3,8 @@
 from coltrane import initialize
 from django.core.management import execute_from_command_line
 
-from os import getenv
 
-env = {
-    "GITHUB_USERNAME": getenv("GITHUB_USERNAME"),
-    "GITHUB_PERSONAL_ACCESS_TOKEN": getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
-}
-
-wsgi = initialize(ENV=env)
+wsgi = initialize()
 
 if __name__ == "__main__":
     execute_from_command_line()
