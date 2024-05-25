530b0c267775306707bd5420c790b256ff565800
adamghill <adamghill@yahoo.com>
2024-05-24 23:01:36 -0400

    Might need the actual env for a little longer.

--- site/app.py
+++ site/app.py
@@ -3,7 +3,14 @@
 from coltrane import initialize
 from django.core.management import execute_from_command_line
 
-wsgi = initialize()
+from os import getenv
+
+env = {
+    "GITHUB_USERNAME": getenv("GITHUB_USERNAME"),
+    "GITHUB_PERSONAL_ACCESS_TOKEN": getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
+}
+
+wsgi = initialize(ENV=env)
 
 if __name__ == "__main__":
     execute_from_command_line()
