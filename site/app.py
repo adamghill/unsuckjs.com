af5d85d0fe8b6d1107961ae52170680efba7176f
adamghill <adamghill@yahoo.com>
2024-05-25 22:58:43 -0400

    Use features in coltrane==0.35.4.

--- site/app.py
+++ site/app.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 
-from coltrane import initialize
-from django.core.management import execute_from_command_line
-from sys import argv
+from coltrane import initialize, run
 
+wsgi = initialize()
 
-is_compress_offline = False
-
-if len(argv) > 1 and argv[1] == "compress":
-    is_compress_offline = True
-
-wsgi = initialize(COMPRESS_OFFLINE=is_compress_offline)
-
-if __name__ == "__main__":    
-    execute_from_command_line()
+if __name__ == "__main__":
+    run()
