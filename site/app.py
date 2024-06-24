6555a5516df36a5442c647d51cfcc71529618364
adamghill <adamghill@yahoo.com>
2024-06-24 18:19:52 -0400

    Try adding in `refreshcss`.

--- site/app.py
+++ site/app.py
@@ -2,7 +2,16 @@
 
 from coltrane import initialize, run
 
-wsgi = initialize()
+COMPRESS_FILTERS = {
+    "css": [
+        "refreshcss.filters.RefreshCSSFilter",
+        "compressor.filters.css_default.CssAbsoluteFilter",
+        "compressor.filters.cssmin.rCSSMinFilter",
+    ],
+    "js": ["compressor.filters.jsmin.rJSMinFilter"],
+}
+
+wsgi = initialize(COMPRESS_FILTERS=COMPRESS_FILTERS, COMPRESS_ENABLED=True)
 
 if __name__ == "__main__":
     run()
